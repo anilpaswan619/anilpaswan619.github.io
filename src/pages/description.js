4317441a2d1d59bf8e26a440e3438ab280b6e049
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2024-09-09 11:09:55 +0530

    updated

--- src/pages/description.js
+++ src/pages/description.js
@@ -3,26 +3,50 @@ import { Card } from "react-bootstrap";
 
 const Description = () => {
   return (
-    <div className="container empty-space-40 ">
-      <h1>About Me</h1>
-      <div className="row">
-        <div className="col-md-6">
-          <Card>
-            <Card.Body>
-              <Card.Title>Experience</Card.Title>
-              <Card.Text>{/* Add your experience details here */}</Card.Text>
+    <div className="container empty-space-40">
+      <h3 className="fw-bold pb-5 about-me-heading">About Me</h3>
+      <div className="row my-5 justify-content-center gap-5">
+        <div className="col-md-4">
+          <Card className="rounded-4 border-2 border-black position-relative">
+            <Card.Body className="text-center p-4">
+              <div className="icon-container position-absolute top-0 start-50 translate-middle">
+                <i className="bi bi-award-fill fs-2 border border-3 rounded-5 border-black p-1 bg-white"></i>
+              </div>
+              <Card.Title className="fw-bold my-4">Experience</Card.Title>
+              <Card.Text className="text-muted">
+                <p>6+ years in Frontend & UI Development</p>
+                <p>Senior Software Engineer</p>
+              </Card.Text>
             </Card.Body>
           </Card>
         </div>
-        <div className="col-md-6">
-          <Card>
-            <Card.Body>
-              <Card.Title>Education</Card.Title>
-              <Card.Text>{/* Add your education details here */}</Card.Text>
+        <div className="col-md-4">
+          <Card className="rounded-4 border-2 border-black position-relative">
+            <Card.Body className="text-center p-4">
+              <div className="icon-container position-absolute top-0 start-50 translate-middle">
+                <i className="bi bi-laptop-fill fs-2 border border-3 rounded-5 border-black p-1 bg-white"></i>
+              </div>
+              <Card.Title className="fw-bold my-4">Education</Card.Title>
+              <Card.Text className="text-muted">
+                <p>Master of Technology - (Computer Science)</p>
+                <p>Bachelor of Technology - (Information Technology)</p>
+              </Card.Text>
             </Card.Body>
           </Card>
         </div>
       </div>
+
+      <p className="fw-bold text-black-50 description-text py-5">
+        I am a Senior Software Engineer with 6+ years of experience in Frontend
+        & UI Development. I have a Master of Technology in Computer Science and
+        a Bachelor of Technology in Information Technology. I am passionate
+        about creating responsive, user-friendly, and visually appealing web
+        applications. I have a strong foundation in web technologies and
+        programming languages such as HTML, CSS, JavaScript, React, and Angular.
+        I am seeking new opportunities to work on challenging projects that
+        allow me to apply my skills and expertise to create innovative web
+        solutions.
+      </p>
     </div>
   );
 };
