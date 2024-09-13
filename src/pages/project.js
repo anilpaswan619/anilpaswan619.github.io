a6e15a8efc2cd6f60fa5c4809b6edf2727f151c5
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2024-09-13 22:32:54 +0530

    updated

--- src/pages/project.js
+++ src/pages/project.js
@@ -6,6 +6,170 @@ const Project = () => {
       <h3 className="fw-bold title-heading mb-5">
         Project <span>Highlights</span>
       </h3>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card border-0 rounded-4 shadow-lg">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <img
+                    src="https://via.placeholder.com/500"
+                    className="img-fluid rounded-4"
+                    alt="Project"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <h5 className="fw-bold">Project Title</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    euismod, nisl nec aliquet lacinia, mi turpis pulvinar
+                    libero, at lacinia elit odio sed est. Nullam nec nunc
+                    scelerisque, interdum lorem eget, luctus metus.
+                  </p>
+                  <a href="#" className="btn btn-primary rounded-pill">
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card border-0 rounded-4 shadow-lg">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <img
+                    src="https://via.placeholder.com/500"
+                    className="img-fluid rounded-4"
+                    alt="Project"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <h5 className="fw-bold">Project Title</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    euismod, nisl nec aliquet lacinia, mi turpis pulvinar
+                    libero, at lacinia elit odio sed est. Nullam nec nunc
+                    scelerisque, interdum lorem eget, luctus metus.
+                  </p>
+                  <a href="#" className="btn btn-primary rounded-pill">
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card border-0 rounded-4 shadow-lg">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <img
+                    src="https://via.placeholder.com/500"
+                    className="img-fluid rounded-4"
+                    alt="Project"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <h5 className="fw-bold">Project Title</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    euismod, nisl nec aliquet lacinia, mi turpis pulvinar
+                    libero, at lacinia elit odio sed est. Nullam nec nunc
+                    scelerisque, interdum lorem eget, luctus metus.
+                  </p>
+                  <a href="#" className="btn btn-primary rounded-pill">
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card border-0 rounded-4 shadow-lg">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <img
+                    src="https://via.placeholder.com/500"
+                    className="img-fluid rounded-4"
+                    alt="Project"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <h5 className="fw-bold">Project Title</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    euismod, nisl nec aliquet lacinia, mi turpis pulvinar
+                    libero, at lacinia elit odio sed est. Nullam nec nunc
+                    scelerisque, interdum lorem eget, luctus metus.
+                  </p>
+                  <a href="#" className="btn btn-primary rounded-pill">
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card border-0 rounded-4 shadow-lg">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <img
+                    src="https://via.placeholder.com/500"
+                    className="img-fluid rounded-4"
+                    alt="Project"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <h5 className="fw-bold">Project Title</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    euismod, nisl nec aliquet lacinia, mi turpis pulvinar
+                    libero, at lacinia elit odio sed est. Nullam nec nunc
+                    scelerisque, interdum lorem eget, luctus metus.
+                  </p>
+                  <a href="#" className="btn btn-primary rounded-pill">
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card border-0 rounded-4 shadow-lg">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <img
+                    src="https://via.placeholder.com/500"
+                    className="img-fluid rounded-4"
+                    alt="Project"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <h5 className="fw-bold">Project Title</h5>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    euismod, nisl nec aliquet lacinia, mi turpis pulvinar
+                    libero, at lacinia elit odio sed est. Nullam nec nunc
+                    scelerisque, interdum lorem eget, luctus metus.
+                  </p>
+                  <a href="#" className="btn btn-primary rounded-pill">
+                    View Project
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
