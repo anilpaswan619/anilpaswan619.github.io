3ae1bf0d7af1bbbd81c45a7d1a7bf0da0e16f71c
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2024-09-20 10:54:41 +0530

    updated

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,44 +2,43 @@ import React from "react";
 
 const Skills = () => {
   const frontendTechnologies = [
-    { name: "HTML/HTML5", level: 90 },
-    { name: "CSS/CSS3", level: 85 },
-    { name: "SCSS", level: 80 },
+    { name: "HTML/HTML5", level: 95 },
+    { name: "CSS/CSS3", level: 90 },
+    { name: "SCSS", level: 85 },
     { name: "Bootstrap", level: 85 },
-    { name: "Tailwind", level: 75 },
+    { name: "Tailwind", level: 80 },
     { name: "JavaScript", level: 90 },
-    { name: "Typescript", level: 70 },
+    { name: "Typescript", level: 80 },
     { name: "React", level: 85 },
-    { name: "Angular", level: 70 },
+    { name: "Angular", level: 80 },
   ];
   const uiTechnologies = [
     { name: "Adobe XD", level: 80 },
     { name: "Figma", level: 85 },
     { name: "Zeplin", level: 75 },
-    { name: "UI/UX Designing", level: 90 },
     { name: "Mockup", level: 80 },
     { name: "Prototyping", level: 85 },
     { name: "Wireframing", level: 80 },
   ];
 
   return (
-    <div className="container empty-space-40">
+    <div className="container my-5">
       <h3 className="fw-bold title-heading mb-5 text-center">
         Skill <span>Set</span>
       </h3>
-      <div className="row my-5">
-        <div className="col-md-6">
-          <div className="card border-0 rounded-4 shadow-lg project-card skill-card h-100">
+      <div className="row">
+        <div className="col-md-6 mb-4">
+          <div className="card border-0 rounded-4 shadow-lg skill-card h-100">
             <div className="card-body">
-              <h4 className="fw-bold mb-3 text-center">
+              <h4 className="fw-bold mb-4 text-center">
                 Frontend Technologies
               </h4>
               <ul className="list-group list-group-flush">
                 {frontendTechnologies.map((technology) => (
                   <li key={technology.name} className="list-group-item">
                     <div className="d-flex align-items-center justify-content-between">
-                      <span className="fw-bold m-2">{technology.name}</span>
-                      <div className="progress-container">
+                      <span className="fw-bold">{technology.name}</span>
+                      <div className="progress-container d-flex align-items-center">
                         <div
                           className="progress-bar"
                           role="progressbar"
@@ -49,6 +48,7 @@ const Skills = () => {
                           aria-valuemax="100"
                         ></div>
                       </div>
+                      <span className="ms-4">{technology.level}%</span>
                     </div>
                   </li>
                 ))}
@@ -56,16 +56,16 @@ const Skills = () => {
             </div>
           </div>
         </div>
-        <div className="col-md-6">
-          <div className="card border-0 rounded-4 shadow-lg project-card skill-card h-100">
+        <div className="col-md-6 mb-4">
+          <div className="card border-0 rounded-4 shadow-lg skill-card h-100">
             <div className="card-body">
-              <h4 className="fw-bold mb-3 text-center">UI Technologies</h4>
+              <h4 className="fw-bold mb-4 text-center">UI Technologies</h4>
               <ul className="list-group list-group-flush">
                 {uiTechnologies.map((technology) => (
                   <li key={technology.name} className="list-group-item">
                     <div className="d-flex align-items-center justify-content-between">
-                      <span className="fw-bold m-2">{technology.name}</span>
-                      <div className="progress-container">
+                      <span className="fw-bold">{technology.name}</span>
+                      <div className="progress-container d-flex align-items-center">
                         <div
                           className="progress-bar"
                           role="progressbar"
@@ -75,6 +75,7 @@ const Skills = () => {
                           aria-valuemax="100"
                         ></div>
                       </div>
+                      <span className="ms-4">{technology.level}%</span>
                     </div>
                   </li>
                 ))}
