a9b8ac23cb8fbc18c7d7a0e5bfb0c3a320a7f9f6
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2024-09-29 12:09:22 +0530

    updated

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,20 +7,21 @@ const Contact = () => {
         Contact <span>Me</span>
       </h3>
       <div className="contact-info text-center mb-5">
-        <p>
-          <i className="bi bi-envelope"></i> Email:{" "}
-          <a href="mailto:anilpaswan619@gmail.com">anilpaswan619@gmail.com</a>
-        </p>
-        <p>
-          <i className="bi bi-linkedin"></i> LinkedIn:{" "}
-          <a
-            href="https://www.linkedin.com/in/anilpaswan619/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Anil Paswan
-          </a>
-        </p>
+        <p class="fs-4 my-4">Find me on the following platforms! </p>
+        <a
+          href="mailto:anilpaswan619@gmail.com"
+          className="btn contact-btn w-50 mx-auto my-4"
+        >
+          <i className="bi bi-envelope"></i> Email: anilpaswan619@gmail.com
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anilpaswan619/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn contact-btn w-50 mx-auto my-4"
+        >
+          <i className="bi bi-linkedin"></i> LinkedIn: Anil Paswan
+        </a>
       </div>
     </div>
   );
